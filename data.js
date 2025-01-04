f779d0fea32deae6bb2932e3cdb4bd32a70eca01
Hindi-Design-Studios <hindidesignstudio.net@gmail.com>
2025-01-04 21:30:50 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,109 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-he-copy-2",
+      "name": "he copy 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.514896226027651,
+        "pitch": -0.032146399528652836,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.514896226027651,
+          "pitch": -0.032146399528652836,
+          "rotation": 0,
+          "target": "1-he-copy"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.508969980130888,
+          "pitch": -0.19849425577650948,
+          "title": "Daylight View",
+          "text": "This is our Daylight view (Interiors)"
+        }
+      ]
+    },
+    {
+      "id": "1-he-copy",
+      "name": "he copy",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.5547027001536984,
+        "pitch": 0.027836381407858823,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4898836524555623,
+          "pitch": 0.05775944335418437,
+          "rotation": 0,
+          "target": "0-he-copy-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.489585479844541,
+          "pitch": -0.10879438758006899,
+          "title": "Night View&nbsp;",
+          "text": "This is our night view interiors."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
